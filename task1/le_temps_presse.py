80fc5099e02f7e6aa272824ea3d093d02f00df10
Tiffany M <margelle@users.noreply.github.com>
2024-01-14 02:03:01 -0500

    add slices and iron

--- task1/le_temps_presse.py
+++ task1/le_temps_presse.py
@@ -1,5 +1,6 @@
 import svg
-from random import choice
+import math
+#from random import choice
 #TODO draw an iron 
 #TODO remove sections of circles
 
@@ -20,33 +21,53 @@ colors = [
 ]
 
 
-def rainbow_circles(x, y):
+def rainbow_circles(x, y, j):
     num_colors = len(colors)
+    angle = (j+1)*2
     for i,color in enumerate(colors):
+        #dash array and dash offset part adapted from https://stackoverflow.com/a/59232930
+        circumference = 2 * math.pi * radius * (num_colors-i)/num_colors
+        offset = 0.25*circumference
+        dash = (angle / 360) * circumference         
         yield svg.Circle(
             cx=x, cy=y, r=radius*(num_colors-i)/num_colors,
-            fill=color,
+            stroke=color, stroke_width=1,
+            fill="transparent", 
+            stroke_dasharray=str(dash)+" "+str(circumference-dash),
+            stroke_dashoffset=str(offset),
         )
 
-#    yield svg.Circle(
-#        cx=x, cy=y, r=radius*5//7,
-#        fill='none',
-#        stroke=colors[2],
-#        stroke_width=radius//7,
-#    )
-
+def iron():
+    #drawn using https://codepen.io/anthonydugois/pen/mewdyZ
+    yield svg.Path(
+        d=[
+            svg.M(600,300),
+            svg.L(350,300),
+            svg.Q(350,200,600,200),
+            svg.Q(550,150,450,150),
+            svg.Q(525,125,600,150),            
+            svg.L(600,200),
+            svg.L(600,300),
+        ],
+        fill="none",
+        stroke="black",
+        stroke_width=5,
+        )
 
 circles = []
 step_x = width // columns
 step_y = height // rows
-for x in range(step_x // 2, width, step_x):
-    for y in range(step_y // 2, height, step_y):
-        circles.extend(rainbow_circles(x=x, y=y))
+for y in range(step_y // 2, height, step_y):        
+    for x in range(step_x // 2, width, step_x):    
+        circles.extend(rainbow_circles(x=x, y=y, j=len(circles)))
 
-
+irons=iron()
 canvas = svg.SVG(
     viewBox=svg.ViewBoxSpec(0, 0, width, height),
     elements=circles,
 )
-
+#canvas = svg.SVG(
+#    viewBox=svg.ViewBoxSpec(0, 0, 800, 600),
+#    elements=irons,
+#)
 print(canvas)
